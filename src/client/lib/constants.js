fd61537a728db03771753daa7cd377df1c12239b
VV01K <wkurtsev@gmail.com>
2016-02-18 00:51:35 +0100

    Add Meteor src

---
+++ src/client/lib/constants.js
@@ -0,0 +1,10 @@
+// Define App Constants
+
+if (Meteor.App) {
+  throw new Meteor.Error('Meteor.App already defined? see client/lib/constants.js');
+}
+
+Meteor.App = {
+  NAME: 'Days',
+  DESCRIPTION: 'Drivers At Your Service (Days) VTC application officielle'
+};
---
+++ src/lib/router.js
@@ -0,0 +1,41 @@
+FlowRouter.route('/', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'home' });
+  }
+});
+
+FlowRouter.route('/reservation', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'reservation' });
+  }
+});
+
+FlowRouter.route('/services', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'services' });
+  }
+});
+
+FlowRouter.route('/drivers', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'drivers' });
+  }
+});
+
+FlowRouter.route('/vehicles', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'vehicles' });
+  }
+});
+
+FlowRouter.route('/values', {
+  action: function() {
+    BlazeLayout.render('layout', { content: 'values' });
+  }
+});
+
+// FlowRouter.route('/post/:slug', {
+//   action: function() {
+//     BlazeLayout.render('layout', { content: 'post' });
+//   }
+// });
