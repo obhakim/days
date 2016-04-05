129c9846a419f709c72fb3ba87f34486f38bc283
wkurtsev <wkurtsev@live.com>
2016-04-05 21:05:43 +0200

    bat

---
+++ mongo-start.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe" --port 3001 --dbpath "C:\inetpub\Days\src\.meteor\local\db"
---
+++ src/meteor-run.bat
@@ -0,0 +1,2 @@
+echo Meteor Batch Started %DATE:/=-%@%TIME::=-% >> log.txt
+meteor run --production >> log.txt 2>&1
