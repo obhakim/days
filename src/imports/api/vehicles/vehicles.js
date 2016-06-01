c02efa14c1fd9165f6978f99205ccceafef616a1
aamingenius <aamproduct@gmail.com>
2016-06-01 10:50:10 +0100

    simpl commit

--- src/imports/api/vehicles/vehicles.js
+++ src/imports/api/vehicles/vehicles.js
@@ -1,23 +1,3 @@
-// <<<<<<< HEAD
-// import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-// import { Mongo } from 'meteor/mongo';
-
-// export const Vehicles = new Mongo.Collection('vehicles');
-
-// Vehicles.schema = new SimpleSchema({
-//   ownerId: { label: 'Proprietaire', type: String, regEx: SimpleSchema.RegEx.Id },
-//   license: { type: String },
-//   // vehicleType: { label: 'Type de véhicule', type: String, allowedValues: Schema.getVehicleTypes() },
-//   vehicleTypeId: { label: 'Type de véhicule', type: String, regEx: SimpleSchema.RegEx.Id },
-//   // brand
-//   // model
-//   // year
-//   color: { type: String },
-//   'photos.$': { type: String }, // 6 champs : devant avec immat, arrier avec immat, cote gauche, cote droit, interieur devant, interier arrier
-// });
-
-// Vehicles.attachSchema(Vehicles.schema);
-// =======
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Mongo } from 'meteor/mongo';
 
