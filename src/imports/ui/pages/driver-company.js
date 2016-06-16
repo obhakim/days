94be8b0fdbab96557dd4036e53b7d87962b054d6
VV01K <wkurtsev@gmail.com>
2016-06-16 19:29:20 +0200

    TemplateNames update

---
+++ src/imports/ui/pages/driver-company.js
@@ -0,0 +1,35 @@
+import './driver-company.html';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Session } from 'meteor/session';
+import { CONST, SESSION } from '../../common/constants.js';
+
+Template.DriverCompany.helpers({
+});
+
+Template.DriverCompany.events({
+  'submit #form': function driverJoinSubmitForm(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    const data = {
+      field: event.target.field.value,
+    };
+
+    // TODO: Add validation
+
+    Meteor.call('METHOD', data, (error) => {
+      if (error) {
+        Session.set(SESSION.ERROR, error);
+      } else {
+        FlowRouter.go('/s/driver/vehicles'); // TODO : replace with redirection by root name
+      }
+    });
+
+    return false;
+  },
+});
+
+Template.DriverCompany.onRendered(function driverCompanyOnRendered() {
+});
