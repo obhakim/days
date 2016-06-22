23e4adad7f5dc722d582dee882b3285ad7961b9b
VV01K <wkurtsev@gmail.com>
2016-06-22 23:38:07 +0200

    Meteor Update 1.3.3.1

--- src/imports/startup/server/fixtures.js
+++ src/imports/startup/server/fixtures.js
@@ -8,28 +8,32 @@ Meteor.startup(() => {
   // PrePopulate data
   // if (Meteor.isServer && VehicleTypes.find().count() === 0) {
   if (Meteor.isServer && !VehicleTypes.findOne()) {
-    const vtBerline = VehicleTypes.insert({
+    VehicleTypes.insert({
+      _id: 'B',
       name: 'Berline',
       ratePerKm: 2.50,
       ratePerHour: 25,
       rateMin: 10,
       rateMultiplier: 1.2,
     });
-    const vtLuxe = VehicleTypes.insert({
+    VehicleTypes.insert({
+      _id: 'L',
       name: 'Luxe',
       ratePerKm: 3.20,
       ratePerHour: 35,
       rateMin: 15,
       rateMultiplier: 1.2,
     });
-    const vtPremium = VehicleTypes.insert({
+    VehicleTypes.insert({
+      _id: 'P',
       name: 'Premium',
       ratePerKm: 5.00,
       ratePerHour: 50,
       rateMin: 20,
       rateMultiplier: 1.2,
     });
-    const vtVan = VehicleTypes.insert({
+    VehicleTypes.insert({
+      _id: 'V',
       name: 'Van',
       ratePerKm: 4.50,
       ratePerHour: 50,
