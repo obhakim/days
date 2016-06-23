fe9f28f7641db820876cb98bf64dafb727315561
aamingenius <aamproduct@gmail.com>
2016-06-23 13:45:00 +0000

    mise ajour simple

--- src/imports/api/models/server/publications.js
+++ src/imports/api/models/server/publications.js
@@ -4,18 +4,27 @@ import { Meteor } from 'meteor/meteor';
 
 import { Models } from '../Models.js';
 
-Meteor.publish('models', function models() {
-  
-  var distinctEntries = _.uniq(Models.find({}, {
-    sort: {brand: 1}, fields: {brand: true}
-}).fetch().map(function(x) {
-    return x.brand;
-}), true);
-    return distinctEntries;
-  // return Models.find()
-});
+Meteor.publish('models', function publishModels() {
+    return Models.distinct( "brand" );
+// });
+// Meteor.publish('models', function publishModels() {
+//     return Models.distinct({}, {sort: {votes: -1, submitted: -1});
+// });
 // var distinctEntries = _.uniq(Models.find({}, {
 //     sort: {myField: 1}, fields: {myField: true}
 // }).fetch().map(function(x) {
 //     return x.myField;
 // }), true);
+// Meteor.publish('brands', function publishBrands() {
+
+//   return Models.distinct('brand');
+
+// });
+
+// Meteor.publish('models', function publishModels($brand) {
+
+//   // return Models.distinct('model',{ brand : $brand });
+
+//   return Models.find({ brand:$brand },{ _id: 0, brand: 0 }); 
+
+// });
