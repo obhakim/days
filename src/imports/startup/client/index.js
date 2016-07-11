21e5eb09320246fa3e54c664ceba67482f6f0afb
younespro <hilaliyounes.dev@gmail.com>
2016-07-11 04:29:42 -0700

    head.html

--- src/imports/startup/client/index.js
+++ src/imports/startup/client/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import './useraccounts-configuration.js';
-import './router.js';
-import './startup.js';
-=======
 import './useraccounts-configuration.js';
 import './router.js';
 import './uploader.js';
 import './startup.js';
->>>>>>> origin/master
+
