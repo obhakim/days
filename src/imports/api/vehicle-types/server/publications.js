33b246c03cd41809c3089bf8acce90dc4b7db45b
ouabou abdelhakim <hakim.ouabou@gmail.com>
2016-07-19 16:19:54 +0100

    reset merge changes

--- src/imports/api/vehicle-types/server/publications.js
+++ src/imports/api/vehicle-types/server/publications.js
@@ -1,11 +1,8 @@
-/* eslint-disable prefer-arrow-callback */
-
 import { Meteor } from 'meteor/meteor';
-
 import { VehicleTypes } from '../vehicle-types.js';
 
 Meteor.publish('vehicletypes', function vehicletypes() {
   return VehicleTypes.find();
-});
 
+});
 
