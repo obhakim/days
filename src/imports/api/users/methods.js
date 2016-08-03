8494cac565e4d0489d0a35a02956ef6cd12a20ae
ouabou abdelhakim <hakim.ouabou@gmail.com>
2016-08-03 23:26:52 +0100

    annulation des modifs

--- src/imports/api/users/methods.js
+++ src/imports/api/users/methods.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
 import { CONST } from '../../common/constants.js';
+import { check } from 'meteor/check';
+import { Users } from './users.js';
 
 // new validated-method style
 
@@ -49,59 +51,43 @@ import { CONST } from '../../common/constants.js';
 Meteor.methods({
   updateUserProfile: (newProfile) => {
     const userId = Meteor.userId();
+    // var isEmailChanged = currentProfile ?
+    //     newProfile.email != currentProfile.email :
 
-    if ((newProfile.creditCard.num.length === 0)
-      || (newProfile.creditCard.validThruM.length === 0)
-      || (newProfile.creditCard.validThruY.length === 0)
-      || (newProfile.creditCard.cvv.length === 0)
-      || (newProfile.creditCard.name.length === 0)) {
+    Meteor.users.update(userId, {
+      $set: {
+        profile: newProfile,
+      },
+    }, {
+      validationContext: 'updateUserProfile',
+    });
 
-      throw new Meteor.Error(400, "les informations de la carte bancaire are required");
+    // if (Meteor.isServer) {
+    //   this.unblock()
+    //   if (isEmailChanged) {
+    //     // Send notification
+    //     try {
+    //       Accounts.sendVerificationEmail()
+    //     } catch (error) {
+    //       //throw error
+    //       console.log(error)
+    //     }
+    //   }
+    // }
 
-    } else if ((newProfile.street.length === 0)
-      || (newProfile.city.length === 0)
-      || (newProfile.zipcode.length === 0)) {
-
-      throw new Meteor.Error(400, "les informations de l'adresse are required");
-
-    } else {
-      Meteor.users.update(userId, {
-        $set: {
-          profile: newProfile,
-        },
-      }, {
-        validationContext: 'updateUserProfile',
-      });
-    }
+  // return id
   },
   // Need this to force Driver role
   createDriver: (newUser) => {
+    check(newUser, Users.schema.namedContext('createDriver'));
 
-    if ((newUser.profile.creditCard.num.length === 0)
-      || (newUser.profile.creditCard.validThruM.length === 0)
-      || (newUser.profile.creditCard.validThruY.length === 0)
-      || (newUser.profile.creditCard.cvv.length === 0)
-      || (newUser.profile.creditCard.name.length === 0)) {
+    const userId = Accounts.createUser({
+      username: newUser.username,
+      email: newUser.email,
+      password: newUser.password,
+      profile: newUser.profile,
+    });
 
-      throw new Meteor.Error(400, "les informations de la carte bancaire are required");
-
-    } else if ((newUser.profile.street.length === 0)
-      || (newUser.profile.city.length === 0)
-      || (newUser.profile.zipcode.length === 0)) {
-
-      throw new Meteor.Error(400, "les informations de l'adresse are required");
-
-    } else {
-      const userId = Accounts.createUser({
-        username: newUser.username,
-        email: newUser.email,
-        password: newUser.password,
-        profile: newUser.profile,
-      });
-
-      Roles.addUsersToRoles(userId, CONST.USER_ROLES.DRIVER);
-
-    }
-
+    Roles.addUsersToRoles(userId, CONST.USER_ROLES.DRIVER);
   },
 });
