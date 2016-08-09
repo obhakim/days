6dcef472ba27fc8fe0bcc4e24f2c257d33c48125
Vlad <wkurtsev@gmail.com>
2016-08-09 18:29:07 +0200

    Search (not finished)

--- src/imports/api/reservations/server/publications.js
+++ src/imports/api/reservations/server/publications.js
@@ -1,14 +1,33 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Match } from 'meteor/check';
 import { Roles } from 'meteor/alanning:roles';
 import { Reservations } from '../reservations.js';
 import { CONST } from '../../../common/constants.js';
 
-Meteor.publish('reservations', function () {
-  if (Roles.userIsInRole(this.userId, CONST.USER_ROLES.DRIVER)) {
-    return Reservations.find({},
-      { sort: { createdAt: -1 } });
-  } else {
-    return Reservations.find({ ownerId: this.userId },
-      { sort: { createdAt: -1 } });
+Meteor.publish('reservations', function (search) {
+  check(search, Match.OneOf(String, null, undefined));
+
+  let query = {};
+  let projection = { limit: 10, sort: { createdAt: -1 } };
+
+  if (search) {
+    const regex = new RegExp(search, 'i');
+
+    query = {
+      $or: [
+        { title: regex },
+        { artist: regex },
+        { year: regex },
+      ],
+    };
+
+    projection.limit = 100;
   }
+
+  if (Roles.userIsInRole(this.userId, CONST.USER_ROLES.CLIENT)) {
+    query.$and = { ownerId: this.userId };
+  }
+
+  return Reservations.find(query, projection);
 });
--- src/imports/ui/pages/reservations.js
+++ src/imports/ui/pages/reservations.js
@@ -1,15 +1,62 @@
 import './reservations.html';
-import { Template } from 'meteor/templating';
-import { Reservations } from '../../api/reservations/reservations.js';
-
 import '../components/reservation-item.js';
+
+import { $ } from 'meteor/jquery';
+import { moment } from 'meteor/momentjs:moment';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Reservations } from '../../api/reservations/reservations.js';
 import { CONST } from '../../common/constants.js';
 
+//https://themeteorchef.com/snippets/simple-search/
 
 Template.Reservations.onCreated(function reservationsPageOnCreated() {
-  this.autorun(() => {
-    this.subscribe('reservations');
+  const instance = Template.instance();
+
+  instance.searchQuery = new ReactiveVar();
+  instance.searching = new ReactiveVar(false);
+
+  instance.autorun(() => {
+    instance.subscribe('reservations', instance.searchQuery.get(), () => {
+      setTimeout(() => {
+        instance.searching.set(false);
+      }, 300);
+    });
   });
+});
+
+Template.Reservations.helpers({
+  searching() {
+    return Template.instance().searching.get();
+  },
+  query() {
+    return Template.instance().searchQuery.get();
+  },
+  reservations() {
+    const albums = Reservations.find();
+    if (albums) {
+      return albums;
+    }
+  },
+});
+
+Template.Reservations.events({
+  'click #recherche'(event, instance) {
+    const value = event.target.value.trim();
+
+    
+
+    instance.searchQuery.set(value);
+
+    if (value !== '' && event.keyCode === 13) {
+      instance.searchQuery.set(value);
+      instance.searching.set(true);
+    }
+
+    if (value === '') {
+      instance.searchQuery.set(value);
+    }
+  },
 });
 
 Template.Reservations.events({
@@ -24,35 +71,35 @@ Template.Reservations.events({
   },
 });
 
-Template.Reservations.helpers({
-  reservations: function () {
-    let filter = {};
-    if (Session.get('name') && Session.get('date')) {
-      const startmonth = parseInt(Session.get('month'), 10);
-      const myDate = new Date(Session.get('year'), startmonth - 1, Session.get('day'));
-      const startDay = moment(myDate).startOf('day').toDate();
-      const endDay = moment(myDate).endOf('day').toDate();
-      filter = {
-        $and: [{
-          ownerName: Session.get('name'),
-        }, {
-            createdAt: {
-              $gte: startDay,
-              $lt: endDay,
-            },
-          }],
-      };
-    }
-    return Reservations.find(
-      filter
-      , {
-        sort: {
-          createdAt: -1,
-          status: -1
-        }
-      });
-  },
-});
+// Template.Reservations.helpers({
+//   reservations: function () {
+//     let filter = {};
+//     if (Session.get('name') && Session.get('date')) {
+//       const startmonth = parseInt(Session.get('month'), 10);
+//       const myDate = new Date(Session.get('year'), startmonth - 1, Session.get('day'));
+//       const startDay = moment(myDate).startOf('day').toDate();
+//       const endDay = moment(myDate).endOf('day').toDate();
+//       filter = {
+//         $and: [{
+//           ownerName: Session.get('name'),
+//         }, {
+//           createdAt: {
+//             $gte: startDay,
+//             $lt: endDay,
+//           },
+//         }],
+//       };
+//     }
+//     return Reservations.find(
+//       filter,
+//       {
+//         sort: {
+//           createdAt: -1,
+//           status: -1,
+//         },
+//       });
+//   },
+// });
 
 Template.Reservations.onRendered(function ReservationsOnRendered() {
   this.$('.datetimepicker').datetimepicker({
